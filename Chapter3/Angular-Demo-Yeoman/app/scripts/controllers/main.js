b2fda7c4f46be6c68d5b13394aba5a11622e6454
Thomas Babu <tommy.developer@gmail.com>
2014-02-19 15:31:12 +0800

    Angular Project generated using Yeoman

---
+++ Chapter3/Angular-Demo-Yeoman/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularDemoYeomanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
